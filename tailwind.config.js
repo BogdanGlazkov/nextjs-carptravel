73f8782bfad99801772f143ab72a62a6fc692437
BogdanGlazkov <bglazkov@i.ua>
2023-08-03 13:47:20 +0300

    Config TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1280px",
+    },
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        bgMain: "#020F08BF",
+        bgMobile: "#010A05BF",
+      },
+    },
   },
   plugins: [],
 };
